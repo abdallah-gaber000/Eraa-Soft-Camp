06cd3c1e7c6ac097466a77c7d9f3bf8be4b22f6c
abdallah-gaber000 <abdallahgaber230@gmail.com>
2023-09-27 19:38:36 +0300

    Task_JS

--- session 12/index.js
+++ session 12/index.js
@@ -1,89 +1,97 @@
-// let table = document.createElement("Table");
-// let tr = document.createElement("Tr");
-// let th = document.createElement("Th");
-// let tr1 = document.createElement("Tr");
-// let th1 = document.createElement("Th");
-// table.setAttribute("border", "1");
-// document.body.appendChild(table);
-// table.appendChild(tr);
-// tr.appendChild(th);
-// tr.appendChild(th1);
-// th.textContent = "Name";
+// let img = document.createElement("img");
+// img.setAttribute("src", "1.png");
+// img.setAttribute("alt", "img");
+// document.body.append("img");
+// console.log(img);
 
-// table.appendChild(tr1);
-// tr1.appendChild(th);
-// tr1.appendChild(th1);
-// th1.innerHTML = "Name";
+let table = document.createElement("table");
+let tr1 = document.createElement("tr");
+let tr2 = document.createElement("tr");
 
-// let table1 = document.createElement("Table");
-// // let tr = document.createElement("Tr");
-// let th = document.createElement("Th");
-// table.setAttribute("border", "1");
-// document.body.appendChild(table);
-// table.appendChild(tr);
-// tr.appendChild(th);
-// th.innerHTML = "Name";
+let th1 = document.createElement("th");
+th1.textContent = "name";
 
-function checkWinner(board) {
-  // Check rows
-  for (let i = 0; i < 3; i++) {
-    if (
-      board[i][0] !== "-" &&
-      board[i][0] === board[i][1] &&
-      board[i][0] === board[i][2]
-    ) {
-      return board[i][0];
-    }
-  }
+let th2 = document.createElement("th");
+th2.textContent = "age";
 
-  // Check columns
-  for (let j = 0; j < 3; j++) {
-    if (
-      board[0][j] !== "-" &&
-      board[0][j] === board[1][j] &&
-      board[0][j] === board[2][j]
-    ) {
-      return board[0][j];
-    }
-  }
+let td1 = document.createElement("td");
+td1.textContent = "abdallah";
 
-  // Check diagonals
-  if (
-    board[0][0] !== "-" &&
-    board[0][0] === board[1][1] &&
-    board[0][0] === board[2][2]
-  ) {
-    return board[0][0];
-  }
-  if (
-    board[0][2] !== "-" &&
-    board[0][2] === board[1][1] &&
-    board[0][2] === board[2][0]
-  ) {
-    return board[0][2];
-  }
+let td2 = document.createElement("td");
+td2.textContent = "21";
+// table.style.border = "1px";
+table.setAttribute("border", "1");
+table.append(tr1);
+table.append(tr2);
 
-  // No winner
-  return "No winner";
-}
+tr1.append(th1);
+tr1.append(th2);
 
-const board1 = [
-  ["X", "O", "X"],
-  ["O", "X", "O"],
-  ["O", "X", "X"],
-];
-console.log(checkWinner(board1)); // Output: X
+tr2.append(td1);
+tr2.append(td2);
+console.log(table);
+document.body.append(table);
 
-const board2 = [
-  ["O", "O", "X"],
-  ["X", "X", "O"],
-  ["O", "O", "O"],
-];
-console.log(checkWinner(board2)); // Output: O
+// function checkWinner(board) {
+//   // Check rows
+//   for (let i = 0; i < 3; i++) {
+//     if (
+//       board[i][0] !== "-" &&
+//       board[i][0] === board[i][1] &&
+//       board[i][0] === board[i][2]
+//     ) {
+//       return board[i][0];
+//     }
+//   }
 
-const board3 = [
-  ["X", "O", "X"],
-  ["O", "X", "X"],
-  ["O", "X", "X"],
-];
-console.log(checkWinner(board3));
+//   // Check columns
+//   for (let j = 0; j < 3; j++) {
+//     if (
+//       board[0][j] !== "-" &&
+//       board[0][j] === board[1][j] &&
+//       board[0][j] === board[2][j]
+//     ) {
+//       return board[0][j];
+//     }
+//   }
+
+//   // Check diagonals
+//   if (
+//     board[0][0] !== "-" &&
+//     board[0][0] === board[1][1] &&
+//     board[0][0] === board[2][2]
+//   ) {
+//     return board[0][0];
+//   }
+//   if (
+//     board[0][2] !== "-" &&
+//     board[0][2] === board[1][1] &&
+//     board[0][2] === board[2][0]
+//   ) {
+//     return board[0][2];
+//   }
+
+//   // No winner
+//   return "No winner";
+// }
+
+// const board1 = [
+//   ["X", "O", "X"],
+//   ["O", "X", "O"],
+//   ["O", "X", "X"],
+// ];
+// console.log(checkWinner(board1)); // Output: X
+
+// const board2 = [
+//   ["O", "O", "X"],
+//   ["X", "X", "O"],
+//   ["O", "O", "O"],
+// ];
+// console.log(checkWinner(board2)); // Output: O
+
+// const board3 = [
+//   ["X", "O", "X"],
+//   ["O", "X", "X"],
+//   ["O", "X", "X"],
+// ];
+// console.log(checkWinner(board3));
---
+++ session 13/session/index.js
@@ -0,0 +1,49 @@
+// //////////////////////////////////////////////////////////////////////////nav and tabs
+// let tabs = document.querySelectorAll(".taps li");
+// let contents = document.querySelectorAll(".content div");
+// tabs.forEach((tab, index) =>
+//   tab.addEventListener("click", () => {
+//     let tabIndex = index;
+//     deactive();
+//     tab.classList.add("active");
+//     contents.forEach((content, index) => {
+//       if (tabIndex == index) {
+//         hiddin();
+//         content.classList.add("show");
+//       }
+//     });
+//   })
+// );
+// function deactive() {
+//   tabs.forEach((tab) => {
+//     tab.classList.remove("active");
+//   });
+// }
+// function hiddin() {
+//   contents.forEach((content) => {
+//     content.classList.remove("show");
+//   });
+// }
+// ///////////////////////////////////////////////////////////////////Alert Module
+// let btn = document.querySelector("button");
+// let text = document.querySelector(".text");
+// let btnclose = document.querySelector(".text_header button");
+// btn.addEventListener("click", (s) => {
+//   //   console.log();
+//   text.style.right = "0%";
+// });
+// btnclose.addEventListener("click", () => {
+//   text.style.right = "-100%";
+// });
+
+/////////////////////////////////////////////////////////////////////offcanves
+// let btn = document.querySelector("button");
+// let offcanves = document.querySelector(".offcanves");
+// let btnclose = document.querySelector(".offcanves_header button");
+// btn.addEventListener("click", () => {
+//   offcanves.style.left = "0%";
+// });
+// btnclose.addEventListener("click", () => {
+//   //   console.log("oo");
+//   offcanves.style.left = "-100%";
+// });
